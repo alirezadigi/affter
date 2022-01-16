3b560a105462a28a423fe65857014aa66d7aa0b0
alirezadigi <47608350+alirezadigi@users.noreply.github.com>
2022-01-16 18:44:15 +0330

    Update affter.sh

--- affter.sh
+++ affter.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade
-sudo apt dist-upgrade
-sudo apt install fish
-sudo apt install vlc
-sudo apt install git
-sudo apt install pip3
-sudo apt install tmux
-sudo apt install qbittorrent
-sudo apt install pv
-sudo apt install locate
-sudo apt install vim
-sudo apt install net-tools
-sudo apt install whois
-sudo apt install traceroute
-sudo apt install bat
-sudo apt install stacer
-sudo apt install iftop
-sudo apt install p7zip-full
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+#sudo apt install fish -y
+sudo apt install vlc -y
+sudo apt install git -y
+sudo apt install python3-pip -y
+sudo apt install tmux -y
+sudo apt install qbittorrent -y
+sudo apt install pv -y
+sudo apt install locate -y
+sudo apt install vim -y
+sudo apt install net-tools -y
+sudo apt install whois -y
+sudo apt install traceroute -y
+sudo apt install bat -y
+sudo apt install stacer -y
+sudo apt install iftop -y
+sudo apt install p7zip-full -y
 
-pip3 install howdoi
+pip3 install howdoi requests tinydb flask
 pip3 install bpytop
