86e2ffd115e1b72ce4731d15e9b23d6a6bf9126e
alirezadigi <47608350+alirezadigi@users.noreply.github.com>
2022-01-18 18:07:41 +0330

    Update affter.sh

--- affter.sh
+++ affter.sh
@@ -29,7 +29,7 @@ sudo apt install telegram -y
 
 
 
-pip3 install howdoi requests tinydb flask
+pip3 install howdoi requests tinydb flask kaggle
 pip3 install bpytop jupyter notebook
 sudo apt install jupyter-notebook
 sudo apt install jupyter-core
